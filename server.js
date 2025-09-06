f00200386925b134defbd8586e551d6b462d3614
Marjorie Shigaki <marjorieshigaki@gmail.com>
2025-09-05 22:22:36 -0400

    Ajustes nos testes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require('./app');
 
+app.get('/health', (req, res) => res.status(200).json({ ok: true }));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
